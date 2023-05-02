67c10797f08ae7496184899c5622933097a901ee
Mobility User7 <mobilityuser7@Admins-MacBook-Pro-4.local>
2023-05-02 14:47:07 +0530

    initial commit

---
+++ GitPractice/GitPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitPracticeApp.swift
+//  GitPractice
+//
+//  Created by Mobility User7 on 5/2/23.
+//
+
+import SwiftUI
+
+@main
+struct GitPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
