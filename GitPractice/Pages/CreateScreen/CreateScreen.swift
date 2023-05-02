6d7ae106fe6d18909277beaeee6b85204b6ae8e9
Mobility User8 <mobilityuser8@Admins-MacBook-Pro-3.local>
2023-05-02 15:49:29 +0530

    demo

--- GitPractice/Pages/CreateScreen/CreateScreen.swift
+++ GitPractice/Pages/CreateScreen/CreateScreen.swift
@@ -59,7 +59,7 @@ struct CreateScreen: View {
                     }
                 
                 Text("Go back")
-                    
+               // kjwdgsfwhj
             }
         }
     }
